f25394595f9e2b8efed386794c0de4f113576c75
Philippe Marschall <philippe.marschall@gmail.com>
2014-10-25 19:00:23 +0200

    bump

---
+++ src/main/java/com/github/marschall/eclipse7693/EscapeCharacter.java
@@ -0,0 +1,16 @@
+package com.github.marschall.eclipse7693;
+
+import static java.lang.System.out;
+
+public class EscapeCharacter {
+
+	public static void main(String[] args) {
+	  // test for https://bugs.eclipse.org/bugs/show_bug.cgi?id=76936
+		out.println("line 1");
+		out.println("fail the test\rwinn"); // winn the test
+		out.println("win not\b\b\byes"); // win yes
+		out.println("line\findented"); // line
+		                               //     indented 
+	}
+
+}
