030f2218a91731051758b09cf87c0c4683290d08
Philippe Marschall <philippe.marschall@gmail.com>
2014-10-25 22:44:57 +0200

    add err

---
+++ src/main/java/com/github/marschall/eclipse7693/Partitions.java
@@ -0,0 +1,13 @@
+package com.github.marschall.eclipse7693;
+
+import static java.lang.System.out;
+import static java.lang.System.err;
+
+public class Partitions {
+
+  public static void main(String[] args) {
+    out.println("out");
+    err.println("err");
+  }
+
+}
