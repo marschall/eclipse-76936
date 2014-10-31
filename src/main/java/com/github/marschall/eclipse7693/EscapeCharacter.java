7340579f76dbf4a64ce31fa1b8aede48c8e51ac9
Philippe Marschall <philippe.marschall@gmail.com>
2014-10-31 10:14:08 +0100

    more backtrack examples, flush and sleep

--- src/main/java/com/github/marschall/eclipse7693/EscapeCharacter.java
+++ src/main/java/com/github/marschall/eclipse7693/EscapeCharacter.java
@@ -4,10 +4,13 @@ import static java.lang.System.out;
 
 public class EscapeCharacter {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 	  // test for https://bugs.eclipse.org/bugs/show_bug.cgi?id=76936
 		out.println("line 1");
 		out.println("fail the test\rwinn"); // winn the test
+//		out.flush();
+//		Thread.sleep(1_000);
+		out.println("win not\b\b\b");
 		out.println("win not\b\b\byes"); // win yes
 		out.println("line\findented"); // line
 		                               //     indented 
