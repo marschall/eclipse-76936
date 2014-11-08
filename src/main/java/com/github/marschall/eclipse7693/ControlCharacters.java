ba95c562b5b43f5b96b545bae15dd20e6769830b
Philippe Marschall <philippe.marschall@gmail.com>
2014-11-08 21:31:54 +0100

    rename class

--- src/main/java/com/github/marschall/eclipse7693/ControlCharacters.java
+++ src/main/java/com/github/marschall/eclipse7693/ControlCharacters.java
@@ -2,7 +2,7 @@ package com.github.marschall.eclipse7693;
 
 import static java.lang.System.out;
 
-public class EscapeCharacter {
+public class ControlCharacters {
 
 	public static void main(String[] args) throws InterruptedException {
 	  // test for https://bugs.eclipse.org/bugs/show_bug.cgi?id=76936
